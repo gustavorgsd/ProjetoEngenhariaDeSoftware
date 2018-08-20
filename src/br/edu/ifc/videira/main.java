3d7f70b344fdb4d7a20e50ef1f6a1153ca4671bf
Gustavo Ribeiro <gustavoribeiro1205@gmail.com>
2018-08-19 21:46:43 -0300

    Criacao do for

--- src/br/edu/ifc/videira/main.java
+++ src/br/edu/ifc/videira/main.java
@@ -7,5 +7,16 @@ import java.util.List;
 public class main {
 	public static void main(String[] args){
 		
+		for(;;) {
+			int op = 0;
+			
+			op = Integer.parseInt(JOptionPane.showInputDialog("Loja de Roupas\n"
+					+ "1 - Comprar produtos\n"
+					+ "2 - Vender produtos\n"
+					+ "3 - Lista de preços\n"
+					+ "4 - Consultar compras\n"
+					+ "5 - Consultar vendas\n"
+					+ "6 - Sair\n"));
+		}
 	}
 }
